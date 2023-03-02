6ef8c91539637ecef0852ac572837faf765a175f
Mikhail Shcherbakov <yuske.dev@gmail.com>
2023-03-02 01:43:44 +0100

    Update scripts

--- scripts/gadgets.static-analysis.js
+++ scripts/gadgets.static-analysis.js
@@ -16,58 +16,39 @@ var argv = require('minimist')(process.argv.slice(2), {
   alias: { l: 'limit' }
 });
 
-const dbPath="../db-app/node_github_lib"
+const dbPath="../db/nodejs"
+const sourcePath="../benchmark-nodejs/lib"
 const queryDir="../codeql/js-queries"
-const reportPath="../raw-data/nodejs-gadgets.md"
-const dataDir="../raw-data/nodejs"
+const propertiesPath="../raw-data/gadgets.dynamic-analysis.csv"
+const reportPath="../raw-data/gadgets.static-analysis.md"
+const dataDir="../raw-data/gadgets.tmp/"
 
-console.info(`Run at ${new Date().toLocaleString()}`);
+if (!fs.existsSync(dataDir)){
+  fs.mkdirSync(dataDir);
+}
+
+const properties = fs.readFileSync(propertiesPath).split("\n");
+
+console.info(`Run the analysis of ${properties.lenght} properties at ${new Date().toLocaleString()}`);
 let timestamp = Date.now();
 
 MarkdownGadgetHeaderReport(reportPath);
 
-const properties = [
-  // "cwd",                      // + | 
-  // "detached",                 // -
-  // "uid",                      // +
-  // "gid",                      // +
-  // "shell",                    // + | +
-  // "argv0",                    // +
-  // "windowsHide",              // -
-  // "windowsVerbatimArguments", // -
-  // "env",                      // + | +
-  // "NODE_V8_COVERAGE",         // + | 
-  // "timeout",                  // - |
-  // "killSignal",               // + |
-  // "input",                    // + | +
-  // "output",                   // - |
-  // "errmap",                   // -
-
-  // "main",                     // +? ModuleWrap, resolve, internalModuleReadJSON, _compileFunction, but all calls via the main package loading 
-  // "1",                        // ? 1345 results (see _compileFunction in loader.js:1073)
-  // "2",                        // - 141 results (url.js:253)
-  // "encoding",                 // - 213 results
-  // "signal",                   // -
-  // "href",                     // +? ESM resolver
-  // "errno",                    // - 8 results
-  // "error",                    // -? 33 results included _compileFunction and resolve, but all looks like FPs
-  // "loaded",                   // - 0 results
-  // "NODE_V8_COVERAGE",         // +
-  // "cachedData",               // + _compileFunction
-
-  // "nullable",                     // - 0 results
-  // "name",                         // -? 404 results (makeContext?)
-  // "origin",                       // -? (makeContext?, HTTP2?)
-  // "codeGeneration",               // -? (makeContext?)
-  // "microtaskMode",                // -? (makeContext?)
-  // "filename",                     // + _compileFunction
-  // "cachedData",                   // + _compileFunction
-  // "contextName",                  // -? (makeContext?)
-  // "contextOrigin",                // -? (makeContext?)
-  // "contextCodeGeneration",         // -? (makeContext?)
-  //"timeout"                     // +? 47 results (we can pollute options.timeout in runInThisContext())
-  "contextExtensions"
-]
+if (!ExistsDb(dbPath)) {
+  try {
+    console.info(`Creating DB ...`);
+    CreateDb(dbPath, sourcePath, {
+      includeTypeScript: true,
+      includeNodeModules: true
+    });
+  }
+  catch(err) {
+    console.error();
+    console.error(`Skip analysing (creating DB):`);
+    console.error(err);
+    process.exit(0);
+  }
+}
 
 const originalQuery = fs.readFileSync(path.join(queryDir, "PrototypePollutingGadget.ql"), "utf-8");
 const modifiedQuery = path.join(queryDir, "PrototypePollutingGadgetMod.ql");
